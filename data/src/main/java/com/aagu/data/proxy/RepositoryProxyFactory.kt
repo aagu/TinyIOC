ceb5a56b82684d059732d85b984445d22befe1ab
aagu <aaguisme@gmail.com>
2020-04-17 13:18:42 +0800

    Data: sql的型参没有标注`@Param`时优先尝试利用kotlin反射获取形参名

--- data/src/main/java/com/aagu/data/proxy/RepositoryProxyFactory.kt
+++ data/src/main/java/com/aagu/data/proxy/RepositoryProxyFactory.kt
@@ -5,6 +5,7 @@ import com.aagu.data.sql.Variable
 import java.lang.reflect.Method
 import java.lang.reflect.Proxy
 import java.util.concurrent.ConcurrentHashMap
+import kotlin.reflect.jvm.kotlinFunction
 
 class RepositoryProxyFactory<T>(private val repositoryInterface: Class<T>) {
     private val methodCache = ConcurrentHashMap<Method, RepositoryMethod>()
@@ -27,13 +28,20 @@ class RepositoryProxyFactory<T>(private val repositoryInterface: Class<T>) {
 
     private fun processArgParameters(method: Method, repositoryMethod: RepositoryMethod) {
         val parameters = method.parameters
+        val kFunction = method.kotlinFunction
+        val kParameters = kFunction?.parameters
         repositoryMethod.argVariables = Array(parameters.size) { Variable() }
         for ((idx, p) in parameters.withIndex()) {
             repositoryMethod.argVariables[idx].type = p.type
             if (p.isAnnotationPresent(Param::class.java)) {
                 repositoryMethod.argVariables[idx].name = p.getAnnotation(Param::class.java).value
             } else {
-                repositoryMethod.argVariables[idx].name = p.name
+                if (kParameters != null) {
+                    // this first element in list should be this callable function
+                    repositoryMethod.argVariables[idx].name = kParameters[idx+1].name
+                } else {
+                    repositoryMethod.argVariables[idx].name = p.name
+                }
             }
         }
     }
